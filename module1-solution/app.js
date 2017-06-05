d0ba7a660e148ff6deab7e356c6e29a50658bf4b
gipsy martinez torrealba <gipsy martinez torrealba>
2017-06-04 20:16:30 -0400

    [Assignment 1]
    - Coursera angularjs course

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,7 @@ app.controller("LunchCheckController", LunchCheckController);
 LunchCheckController.$inject = ['$scope']
 function LunchCheckController($scope){
     $scope.message = '';
+    $scope.messageEmptyItem = '';
     $scope.dishes = '';
     $scope.checkIfTooMuch = function(){
         if($scope.dishes == '') {
@@ -14,7 +15,9 @@ function LunchCheckController($scope){
             return;
         }
         var dishesArray = $scope.dishes.split(',').filter(function(v){return v.trim() !== ''});
+        var count = $scope.dishes.split(',').length
         $scope.message = dishesArray.length > 3 ? 'Too much!' : 'Enjoy!';
+        $scope.messageEmptyItem = count > dishesArray.length ? "I do not consider empty items" : "";
     }
 }
 })();
